cf2e83b2a2a52a3f435c65ec91b77aeb7296a5bd
Rahmasamy <rahmasamy949@gmail.com>
2024-10-21 10:54:09 +0300

    more improvment on php code

--- ShowValidationdata.php
+++ ShowValidationdata.php
@@ -1,19 +1,42 @@
 <?php
 
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $mobile = filter_var($_POST['mobile'], FILTER_SANITIZE_NUMBER_INT);
+
+
+function mobileValidation($mobile){
     if (!preg_match('/^[0-9]{5,20}$/', $mobile)) {
-        exit("Error: Invalid mobile number.");
-    } 
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        return false;
+    }
+    return true;
+}
+
+function emailValidation($email){
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        exit("Error: Invalid email address.");
-    } 
-    echo "Mobile Number: " . htmlspecialchars($mobile) . "<br>";
-    echo "Email: " . htmlspecialchars($email);
+        return false;
+    }
+    return true;
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $mobile = filter_var($_POST['mobile'], FILTER_SANITIZE_NUMBER_INT);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $isMobileValid = mobileValidation($mobile);
+    $isEmailValid = emailValidation($email);
+    if($isMobileValid && $isEmailValid){
+        echo "Mobile Number: " . htmlspecialchars($mobile) . "<br>";
+        echo "Email: " . htmlspecialchars($email);
+    } else {
+        
+        if (!$isMobileValid) {
+            echo "Error: Invalid mobile number.";
+        } elseif (!$isEmailValid) {
+            echo "Error: Invalid email address.";
+        }
+    }
 } else {
     exit("Invalid Request");
 }
